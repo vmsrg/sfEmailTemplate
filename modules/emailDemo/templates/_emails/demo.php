3b46b0e10096fd4ba99d8940b8842142e28247ae
sd <s.daniyev@diansoftware.com>
2017-06-27 22:07:04 +0300

    documentation

--- modules/emailDemo/templates/_emails/demo.php
+++ modules/emailDemo/templates/_emails/demo.php
@@ -1,6 +1,6 @@
 <h1>test email</h1>
 вапвапавп
 авпавп
-<a href=""><?php echo url_for('@game',true); ?></a>
+<a href=""><?php echo url_for('@sfEmailTemplateDemo',true); ?></a>
 
 >>>Вам письмо <?php echo $foo; ?>!!!
